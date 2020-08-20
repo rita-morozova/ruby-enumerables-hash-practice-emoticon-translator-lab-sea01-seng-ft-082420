ee3eff158fb283b0e321caeeda38e816ddd05282
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-20 16:10:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,45 +21,26 @@ end
 
 
 def get_japanese_emoticon(emoticon, symbol)
-<<<<<<< HEAD
- load_library(emoticon).each do |signs, lang|
-    if load_library(emoticon)[signs][:english] == symbol
-       return load_library(emoticon)[signs][:japanese]
-    end 
- end 
-   return "Sorry, that emoticon was not found"
-=======
   emoticon_hash = load_library("./lib/emoticons.yml")
    emoticon_hash.each do |signs, lang|
      if emoticon_hash[signs][:english] == symbol
        emoticon_hash[signs][:japanese]
-       binding.pry 
       else 
         "Sorry, that emoticon was not found"
       end 
     end   
->>>>>>> fc006e11e91fd90903929aeb14c1da3e86719f32
 end
 
 
 def get_english_meaning(emoticon, symbol)
    emoticon_hash= load_library('./lib/emoticons.yml')
-<<<<<<< HEAD
-    emoticon_hash.each_key do |signs|
-      if emoticon_hash[signs][:japanese] == symbol
-       return signs
-      end 
-    end 
-  return "Sorry, that emoticon was not found"
-=======
+  
     emoticon_hash.map do |signs, lang|
       if emoticon_hash[signs][:japanese] == symbol
       
     
       end 
    end 
-  
->>>>>>> fc006e11e91fd90903929aeb14c1da3e86719f32
 end
 
  
